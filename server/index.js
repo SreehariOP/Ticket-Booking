df3e8e35012476e892cd2263ce9398eb875b5096
Sreehari O P <opsreehari96@gmail.com>
2026-03-01 08:38:26 +0530

    Added vercel.json and updated cors

--- server/index.js
+++ server/index.js
@@ -147,12 +147,16 @@ app.post(
 // Apply JSON parser to all other routes
 app.use(express.json());
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   })
+// );
+app.use(cors({
+  origin: [process.env.CLIENT_URL],
+  credentials: true,
+}));
 
 app.use(cookieParser());
 
